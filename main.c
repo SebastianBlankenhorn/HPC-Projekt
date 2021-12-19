e09c6ef86e94f882251426327718772e7e8875d7
Matthias Wohland <mwohland23@gmail.com>
2021-12-19 20:54:19 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct entity
+{
+    char type;
+    int age;
+
+    int fishbreedtime;
+
+    int sharkbreedtime;
+    int starvetime;
+};
+
+int main()
+{
+    struct entity feld[2][1];
+
+    return 0;
+}
